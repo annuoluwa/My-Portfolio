d10164d696e6790713b4c5165aa76871d85cdb23
Elizabeth <zeereigns@hotmail.com>
2025-12-06 16:29:26 +0000

    Change name error

--- about.php
+++ about.php
@@ -5,7 +5,7 @@
     <div id="about">
       <h2>About Me</h2>
       <div class="about-me">
-        <p>Hello! I’m Elizabeth Yomi, a passionate full-stack software engineer who thrives on building functional, user-friendly web applications. 
+        <p>Hello! I’m Elizabeth Osunsanwo, a passionate full-stack software engineer who thrives on building functional, user-friendly web applications. 
         <br>My journey into software development began with an intensive full-stack web development program, where I gained hands-on experience creating modern web applications using React, Node.js, Express, PostgreSQL, and Redux. 
         <br>This training gave me a solid foundation in both frontend and backend development, and taught me how to design scalable, maintainable applications.
 
